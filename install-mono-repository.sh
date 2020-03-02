bb638b098f2a37a51a1190485abe40398fb46bcf
Andrey Rjeutski <filhit@gmail.com>
2020-03-02 16:41:37 +0000

    Install mono

---
+++ install-mono-repository.sh
@@ -0,0 +1,2 @@
+rpmkeys --import "http://pool.sks-keyservers.net/pks/lookup?op=get&search=0x3fa7e0328081bff6a14da29aa6a19b38d3d831ef"
+curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo
---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo ./install-mono-repository.sh
+sudo yum install mono-devel
