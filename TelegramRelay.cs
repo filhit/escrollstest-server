e5dadf25dfd65795b70bf3bdc663969540df0915
Andrey Rjeutski <filhit@gmail.com>
2020-05-23 21:48:01 +0000

    Fix message duplication

--- TelegramRelay.cs
+++ TelegramRelay.cs
@@ -50,30 +50,7 @@ namespace Escrollstest.Server {
         _cancellationTokenSource.Token
       );
 
-      PlayerHooks.PlayerChat += OnPlayerChat;
-      ServerApi.Hooks.ServerJoin.Register (this, OnJoin);
-      ServerApi.Hooks.ServerLeave.Register (this, OnLeave);
       ServerApi.Hooks.ServerBroadcast.Register (this, OnBroadcast);
-    }
-
-    /// <summary>
-    /// Called when a player leaves the server
-    /// </summary>
-    /// <param name="args">event arguments passed by hook</param>
-    private void OnLeave (LeaveEventArgs args) {
-      if (args != null && TShock.Players[args.Who] != null) {
-        SendTelegramMessage ($"{TShock.Players[args.Who].Name} left the game.");
-      }
-    }
-
-    /// <summary>
-    /// Called when a player joins the server
-    /// </summary>
-    /// <param name="args">event arguments passed by hook</param>
-    private void OnJoin (JoinEventArgs args) {
-      if (args != null && TShock.Players[args.Who] != null) {
-        SendTelegramMessage ($"{TShock.Players[args.Who].Name} joined the game.");
-      }
     }
 
     private void OnBroadcast (ServerBroadcastEventArgs args) {
@@ -101,6 +78,18 @@ namespace Escrollstest.Server {
         return;
       }
 
+      if (Regex.IsMatch (args.Message.ToString (), @"Saving world. Momentary lag might result from this.")) {
+        return;
+      }
+
+      if (Regex.IsMatch (args.Message.ToString (), @"World saved.")) {
+        return;
+      }
+
+      if (Regex.IsMatch (args.Message.ToString (), @"^<[^>]+>( sent a sticker.| sent a document.|: .+)")) {
+        return;
+      }
+
       SendTelegramMessage (args.Message.ToString ());
     }
 
@@ -120,13 +109,6 @@ namespace Escrollstest.Server {
           Console.WriteLine (e);
         }
       });
-    }
-
-    void OnPlayerChat (PlayerChatEventArgs args) {
-      if (args.RawText.StartsWith ("/")) {
-        return;
-      }
-      SendTelegramMessage ($"<{args.Player.Name}>: {args.RawText}");
     }
 
     public async Task HandleUpdateAsync (ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) {
@@ -207,9 +189,6 @@ namespace Escrollstest.Server {
     }
 
     private void DeregisterHooks () {
-      PlayerHooks.PlayerChat -= OnPlayerChat;
-      ServerApi.Hooks.ServerJoin.Deregister (this, OnJoin);
-      ServerApi.Hooks.ServerLeave.Deregister (this, OnLeave);
       ServerApi.Hooks.ServerBroadcast.Deregister (this, OnBroadcast);
     }
   }
